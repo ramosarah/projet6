f7b7142ba430dc3adda5a791aa18a29710fa9479
Sarah Ramos <ramosarah@protonmail.ch>
2019-07-09 16:38:37 +0200

    création databse sql

--- script.sql
+++ script.sql
@@ -1,9 +1,3 @@
 
-wireframe
-https://wireframe.cc/QwTmBL
-https://wireframe.cc/afMqLg
-https://wireframe.cc/g2y6at
-https://wireframe.cc/pro/pp/4569f9547259788
+CREATE DATABASE Promenades;
 
-
-base de donnée:
--- www/promenade.php
+++ www/promenade.php
@@ -3,8 +3,19 @@
 
 class Promenade {
 
+    private $id;
+    private $nom;
+    private $titre;
+    private $img;
+    private $pays;
+    private $ville;
+    private $zip;
+    private $depart;
+    private $arrivee;
+    private $desc;
 
 
+    
 
 
 
