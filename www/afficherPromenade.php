4f0d1e80660a2e1b71668be244b8a0dcc756bdeb
Sarah <ramosarah@protonmail.ch>
2019-07-16 11:47:06 +0200

    MaJ font-family dans afficher et modifier

--- www/afficherPromenade.php
+++ www/afficherPromenade.php
@@ -15,7 +15,9 @@ $promenade = $database->afficherPromenade($id);
         
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/jellee-typeface" type="text/css"/> 
         <link rel="stylesheet" type="text/css" href="style.css">
         <title>Afficher une promenade</title>
 
@@ -31,7 +33,7 @@ $promenade = $database->afficherPromenade($id);
         
 
     </head>
-    <body >
+    <body class="form-bg">
 
 
         <div class="imgBg">
