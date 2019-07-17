fa8005e4b7df2f8d1c1505800e7b1d3d844cca2b
Adylitto <adylitto@gmail.com>
2019-07-17 16:39:40 +0200

    function last insert id pour carousel affiche derniere prom créées

--- www/index.php
+++ www/index.php
@@ -79,14 +79,14 @@ $promenade = $database->searchBalade($search);
         </ol>
         <div class="carousel-inner ">
             <div class="carousel-item active">
-                <a href="afficherPromenade.php?id=2">
-                    <img class="d-block w-100 " src="/assets/img/1.jpg" alt="First slide">
+                <a href="afficherPromenade.php?id=<?php echo $promenade->lastInsert()?>">
+                    <img class="d-block w-100 img-fluid hoverable" src="/assets/img/1.jpg" alt="First slide">
                 </a></div>
             <div class="carousel-item">
-                <img class="d-block w-100" src="/assets/img/2.jpg" alt="Second slide">
+                <img class="d-block w-100 img-fluid hoverable" src="/assets/img/2.jpg" alt="Second slide">
             </div>
             <div class="carousel-item">
-                <img class="d-block w-100" src="/assets/img/3.jpg" alt="Third slide">
+                <img class="d-block w-100 img-fluid hoverable" src="/assets/img/3.jpg" alt="Third slide">
             </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
