35490014f7cc689a2504154bc6197d163b9476af
Adylitto <adylitto@gmail.com>
2019-07-17 14:32:38 +0200

    display enhancement

--- www/header.php
+++ www/header.php
@@ -9,18 +9,18 @@ require_once "database.php";
         <button class="navbar-toggler white" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon "></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <div class="collapse navbar-collapse ml-5" id="navbarTogglerDemo01">
             <a class="navbar-brand logo text-white" href="index.php"><i class="fas fa-walking fa-3x"></i></a>
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                 <li class="nav-item active">
-                    <a class="nav-link text-white" href="index.php">Accueil<span class="sr-only">(current)</span></a>
+                    <a class="nav-link text-white" href="index.php"><i class="fas fa-th-list"></i>Liste des promenades<span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="ajouterPromenade.php">Ajouter votre promenade</a>
+                    <a class="nav-link text-white" href="ajouterPromenade.php"><i class="fas fa-highlighter"></i>Ajouter votre promenade</a>
                 </li>
                 
             </ul>
-            <form class="form-inline my-2 my-lg-0" action="index.php" method="GET">
+            <form class="form-inline my-2 my-lg-0 mr-5" action="index.php" method="GET">
                 <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" name="search">
                 <button class="btn btn-outline-success text-white my-2 my-sm-0" type="submit" value="search" >Search</button>
             </form>
--- www/index.php
+++ www/index.php
@@ -1,4 +1,33 @@
 <?php
+// On démarre la session AVANT d'écrire du code HTML
+session_start();
+
+// On s'amuse à créer quelques variables de session dans $_SESSION
+$_SESSION['prenom'] = 'Jean';
+$_SESSION['nom'] = 'Dupont';
+$_SESSION['age'] = 24;
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Titre de ma page</title>
+    </head>
+    <body>
+    <p>
+        Salut <?php echo $_SESSION['prenom']; ?> !<br />
+        Tu es à l'accueil de mon site TIPTOPProm aller sur une autre page ?
+    </p>
+    <p>
+        <a href="mapage.php">Lien vers mapage.php</a><br />
+        <a href="monscript.php">Lien vers monscript.php</a><br />
+        <a href="informations.php">Lien vers informations.php</a>
+    </p>
+    </body>
+</html>
+
+<?php
 require_once 'database.php';
 $database = new Database;
 
